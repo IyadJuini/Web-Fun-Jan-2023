1125aba45f0570e54b978daa11f8743e70159053
unknown <yad.juini@gmail.com>
2023-07-03 17:42:51 +0200

    Likes

---
+++ Week2/Day 2/Core/Likes/script.js
@@ -0,0 +1,12 @@
+var arr = document.querySelectorAll("#numberOfLikes");
+
+function addLikes(index){
+    // var e = document.querySelector("#numberOfLikes");
+    var e = document.querySelectorAll("#numberOfLikes")[index];
+    var likes = parseInt(e.innerText);
+    likes++;
+    e.innerText = likes;
+
+}
+
+console.log(arr[2].innerText);
